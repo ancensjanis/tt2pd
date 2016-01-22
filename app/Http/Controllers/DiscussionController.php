8920e04acdb16246254bccf91a686f33344be5c8
Janis Ancens <ja14015@lu.lv>
2016-01-22 11:45:20 +0200

    kaut ko vēl mazliet salauzu, joprojam nav gatavs

--- app/Http/Controllers/DiscussionController.php
+++ app/Http/Controllers/DiscussionController.php
@@ -11,12 +11,12 @@ class DiscussionController extends Controller
 {
     public function byUser($input) {
         if (empty($input) || !is_int($input)) {
-            //$data = \App\Discussion::all()->sortByDesc('created_at')->first();
+            $data = \App\Discussion::all()->sortByDesc('created_at');
         } else {
-            //$data = \App\User::find($input)->discussions()->sortByDesc('created_at')->first();
+            $data = \App\User::find($input)->discussions()->sortByDesc('created_at');
         }
-        $data = \App\Discussion::all()->first();
-        var_dump($data);
+        //$data = \App\Discussion::all()->first();
+        //var_dump($data);
         return view('discussions', $data);
     }
 }
--- resources/views/discussions.php
+++ resources/views/discussions.php
@@ -7,5 +7,5 @@
 
 @section('content')
     <h1>Discussions</h1>
-    <pre>a</pre>
+
 @stop
